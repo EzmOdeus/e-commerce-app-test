4837d0642e686b8f510e3328fdb86233bf59ddd6
EzmOdeus <87050190+EzmOdeus@users.noreply.github.com>
2023-08-27 10:13:15 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default function Home(props) {
                 </div>
 
                 <a
-                  href="/products"
+                  href="/sign-in"
                   className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700"
                 >
                   Shop Now
